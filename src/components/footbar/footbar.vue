8763f8e16c602417865cb0628ee426d1d90a072b
Abise <1097843969@qq.com>
2017-11-10 15:15:18 +0800

    fix the space problem in footbar

--- src/components/footbar/footbar.vue
+++ src/components/footbar/footbar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="footbar">
     <div class="link_box">
-      <a href="https://www.asch.io" target="_blank">阿希官网</a>&nbsp;|&nbsp;
-      <a href="http://bbs.asch.io" target="_blank">阿希生态社区</a>&nbsp;|&nbsp;
-      <p>官方QQ群：485979564</p>&nbsp;|&nbsp;
+      <a href="https://www.asch.io" target="_blank">阿希官网</a>&nbsp;|
+      <a href="http://bbs.asch.io" target="_blank">阿希论坛</a>&nbsp;|
+      <p>官方QQ群：485979564</p>&nbsp;|
       <p>官方QQ群：545183438</p>
     </div>
     <span>&copy;Asch</span>
